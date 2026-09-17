dcb4d90ea8595f40bd896427c40b9e4d1377c53a
Elise Elf <elise.elf@hotmail.com>
2026-09-17 09:34:57 +0200

    uppgift 1

--- src/main/java/me/ellebelle/Main.java
+++ src/main/java/me/ellebelle/Main.java
@@ -5,5 +5,15 @@ package me.ellebelle;
 public class Main {
     static void main() {
 
+        Product product1 = new Product("Kakaor", 20.50, 8);
+        Product product2 = new Product("Mjölk", 15.50, 9);
+        Product product3 = new Product("Saft", 10.50, 10);
+
+        IO.println(product1.getName());
+        IO.println(product2.getPrice());
+        IO.println(product3.getStockQuantity());
+
+        double produkt1TotValue = product1.totalValue();
+        IO.println(produkt1TotValue);
     }
 }
---
+++ src/main/java/me/ellebelle/Product.java
@@ -0,0 +1,44 @@
+package me.ellebelle;
+
+public class Product {
+    private String name;
+    private double price;
+    private int stockQuantity;
+
+
+    public Product(String name, double price, int stockQuantity) {
+        this.name = name;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+
+
+    public double totalValue() {
+        return price * stockQuantity;
+    }
+
+}
